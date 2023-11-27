35c7d8c95490db5e2a13c98707ee06c0fd3eeae5
dani.diaz <daniel.diaz@institutvidreres.cat>
2023-11-27 14:22:40 +0100

    Base App

--- app/src/main/java/com/sopa/projectevideoappdiaz/MainActivity.kt
+++ app/src/main/java/com/sopa/projectevideoappdiaz/MainActivity.kt
@@ -3,8 +3,6 @@ package com.sopa.projectevideoappdiaz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sopa.projectevideoappdiaz.databinding.ActivityMainBinding
-
-
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
